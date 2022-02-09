2b2b0d47066345d743f7f432e3806a1e6a3eb279
Andrew Porter <mephisto83@hotmail.com>
2022-02-09 17:26:14 -0600

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
-
+import { 
+  getUserSignedIn,
+  getUserSignedInId,
+  setAuthenticationInfo,
+  setCheckedSignedIn 
+} from './features/application/application-slice'
 import { Navbar } from './components/Navbar'
+import { setAuthStateChangedHandler } from './features/firebase-app'
 import { About } from './pages/About'
 import { Home } from './pages/Home'
 
 const App: React.FC = () => {
+  const dispatch = useDispatch();
+  const uid = useSelector(getUserSignedInId);
+  const isSignedIn = useSelector(getUserSignedIn);
+  const policyInformation = useSelector(get_policy_information);
+  setAuthStateChangedHandler((data: any) => {
+    dispatch(setCheckedSignedIn(true))
+    dispatch(setAuthenticationInfo(data))
+  })
+
+
   return (
     <BrowserRouter>
       <Navbar />
@@ -13,6 +30,7 @@ const App: React.FC = () => {
         <Switch>
           <Route path="/" component={Home} exact />
           <Route path="/about" component={About} />
+          {/* Inject_Routes_Here */}
         </Switch>
       </div>
     </BrowserRouter>
--- src/store.ts
+++ src/store.ts
@@ -1,15 +1,18 @@
-import { combineReducers, createStore } from 'redux'
+import { combineReducers, createStore } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { applyMiddleware, compose } from 'redux';
 import { devToolsEnhancer } from 'redux-devtools-extension'
 import { CounterReducer } from './features/counter'
 
 /* Create root reducer, containing all features of the application */
 const rootReducer = combineReducers({
   count: CounterReducer,
+  /*SWAP-SLICES*/
 })
 
-const store = createStore(
-  rootReducer,
-  /* preloadedState, */ devToolsEnhancer({})
-)
+const middleWare = [thunkMiddleware];
+const composeEnhancers = ((window as any)['__REDUX_DEVTOOLS_EXTENSION_COMPOSE__'] as any) || compose;
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middleWare)));
 
-export default store
+
+export default store;
